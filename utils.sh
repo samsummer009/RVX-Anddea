2b348f8c59664ab3ed94a0d31c5e5959b16be4c1
Obasu <samneill852@gmail.com>
2026-03-20 21:33:07 +0500

    Remove incompatible patch-options.json usage
    - Remove --options-file arguments from all configurations
    - Morphe CLI uses different JSON format with 'key' instead of 'patchName'
    - Fix list-versions argument order to match Morphe CLI help output

--- utils.sh
+++ utils.sh
@@ -283,7 +283,7 @@ isoneof() {
 
 patches_list_versions() {
 	local cli_jar=$1 patches_jar=$2 pkg_name=$3 op
-	if ! op=$(java -jar "$cli_jar" list-versions -f "$pkg_name" --patches="$patches_jar" 2>&1); then
+	if ! op=$(java -jar "$cli_jar" list-versions --patches="$patches_jar" -f "$pkg_name" 2>&1); then
 		epr "Could not list versions $cli_jar: '$op'"
 		return 1
 	fi
